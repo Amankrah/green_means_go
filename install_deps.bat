da72d1fbd10b0ec235fb39b080a6eb362d4b37cf
eakwofie <amankrahkwofie354@gmail.com>
2025-12-27 19:44:30 -0500

    fix vulnerability

--- install_deps.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Activating virtual environment...
-call african_lca_api\Scripts\activate.bat
-
-echo Installing Python dependencies...
-pip install -r requirements.txt
-
-echo Dependencies installed successfully!
-pause
